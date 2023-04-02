33dc518d8f1a2e643147cf1d2eae2b3f4f97d473
kannanGitRepo <43965977+kannanGitRepo@users.noreply.github.com>
2023-04-02 22:24:19 +0530

    Initial commit

---
+++ src/main/java/com/example/MyBudget/MyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBudgetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyBudgetApp/MyBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBudgetApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
